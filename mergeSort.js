f3830812b41f8e3c36ba6719dfeeca718593d4f0
Michael Walters <waltersm8@winthrop.edu>
2021-04-09 12:34:51 -0400

    Comments for merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -22,22 +22,22 @@ function merge(arr, l, m, r) {
     let n1 = m -l + 1;
     let n2 = r - m;
 
-    let L = [n1];
+    let L = [n1]; //Creates left and right half arrays with correct num of elements
     let R = [n2];
 
     for (let i = 0; i < n1; i++) {
-        L[i] = arr[l + i];
+        L[i] = arr[l + i]; //Creates array with first half of section
     }
     for (let j = 0; j < n2; j++) {
-        R[j] = arr[m + 1 + j];
+        R[j] = arr[m + 1 + j]; //Creates array with second half of section
     }
 
-    let i = 0;
+    let i = 0; //Resets i and j for for loop
     let j = 0;
 
-    let k = l;
+    let k = l; //Sets k to strating point in main array
 
-    while(i < n1 && j < n2) {
+    while(i < n1 && j < n2) { //Loops through sub arrays, inserting into main array the sub arrays merged together
         if(L[i] <= R[j]) {
             arr[k] = L[i];
             i++
@@ -47,13 +47,13 @@ function merge(arr, l, m, r) {
         k++;
     }
 
-    while (i < n1) {
+    while (i < n1) { //Puts remaining items into main array if any left
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
+    while (j < n2) { //Puts remaining items into main array if any left
         arr[k] = R[j];
         j++;
         k++;
@@ -62,12 +62,14 @@ function merge(arr, l, m, r) {
 
 function sort(arr, l, r) {
     if (l < r) {
-        let m = l + (r - 1)/2;
+        let m = l + (r - 1)/2; //Finds midpoint of array section
 
-        sort(arr, l, m);
-        sort(arr, m+1, r);
+        sort(arr, l, m); //Sorts with bottom half of array
+        sort(arr, m+1, r); //Sorts with top half of array 
 
-        merge(arr, l, m, r);
+        merge(arr, l, m, r); //Once array is broken into singular pieces, they are merged together in order
+    } else {
+        noLoop();
     }
 }
 
